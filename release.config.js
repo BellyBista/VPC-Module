6f3600630b8d4bf418174ee771c1ea29058e73ec
quadribello <quadribello41@gmail.com>
2024-01-13 23:07:00 -0500

    update repoUrl

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     branches: "main",
-    repositoryUrl: "https://github.com/BellyBista/VPC-Module.git",
+    repositoryUrl: "https://github.com/quadri-olamilekan/VPC-Module.git",
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
